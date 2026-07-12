697ac23db3943369741796e5c28330227c9903ee
raghutwins2024-coder <raghu.twins.2024@gmail.com>
2026-07-12 17:11:50 +0530

    Add files via upload

---
+++ [SPIF_team].[MB_TBL_SW_UNITS_APJ]-SELECT.sql
@@ -0,0 +1,24 @@
+USE [SPIF]
+GO
+
+SELECT [SR_BADGE]
+      ,[Sales_BU_ID]
+      ,[QUARTER_ID]
+      ,[VENDOR_NAME]
+      ,[CSPG_ITM_QTY]
+      ,[ORDER_NUMBER]
+      ,[MONTH]
+      ,[WEEK]
+      ,[CSPG_ITM_NBR]
+      ,[CSPG_ITM_DESC]
+      ,[DATE]
+      ,[UPDATE_COMMENT]
+      ,[CREATE_DATE]
+      ,[CREATED_BY]
+      ,[UPDATE_DATE]
+      ,[UPDATED_BY]
+  FROM [SPIF_team].[MB_TBL_SW_UNITS_APJ]
+
+GO
+
+
